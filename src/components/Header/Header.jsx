4d69e6f6a5a04900e3d9db12fe1f12c6bd52ca06
MujuniEli <elijahmujuni@gmail.com>
2024-01-07 21:39:48 +0300

    Added Nav styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from '@material-ui/icons/Search';
+import useStyles from './style'
+
+const Header = () => {
+    const classes = useStyles();
+    return (
+                <AppBar position="static" style={{ background: '#4c97d0' }}>
+                    <Toolbar className={classes.toolbar}>
+                        <Typography variant="h5" className={classes.title}>
+                            Know Your Travel Sites
+                        </Typography>
+                        <Box display="flex">
+                            <Typography variant="h6" className={classes.title}>
+                                Explore different locations!
+                            </Typography>
+                                {/* <Autocomplete> */}
+                                    <div className={classes.search}>
+                                        <div className={classes.searchIcon}>
+                                            <SearchIcon />
+                                        </div>
+                                        <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput}} />
+                                    </div>
+                                {/* </Autocomplete> */}
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+    )
+}
+
+export default Header;
