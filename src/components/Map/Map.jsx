5e2c7088bf734d35a95173a5e1d7ffe3022459c6
MujuniEli <elijahmujuni@gmail.com>
2024-01-07 23:09:41 +0300

    Added google maps

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { LocationOnOutlined } from "@material-ui/icons/LocationOnOutlined";
+import  Rating  from "@material-ui/lab";
+
+import useStyles from './style'
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width: 600px)');
+
+    const coordinates = {Lat:0, Lng: 0};
+    
     return (
-                <h1>Header</h1>
+                <div className={classes.mapContainer}>
+                    <GoogleMapReact
+                        bootstrapURLKeys={{key:'AIzaSyBHE0v9sUrDzjTGv6aqincgJu0g3S1Ebh4'}}
+                        defaultCenter={coordinates}
+                        center={coordinates}
+                        defaultZoom={14}
+                        margin={[50, 50, 50, 50]}
+                        options={''}
+                        onChange={''}
+                        onChildClick={''}
+                    >
+
+                    </GoogleMapReact>
+                </div>
     )
 }
 
